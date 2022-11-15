7d4ac594523c157887c83f99f7467be7d994413b
AlekseyKozlovskiy <kyzinka@yandex.ru>
2022-11-15 03:06:33 +0300

    add Admin:user / Admin:category endpoints

--- server/src/main/java/ru/practicum/explore/categories/CategoryService.java
+++ server/src/main/java/ru/practicum/explore/categories/CategoryService.java
@@ -4,6 +4,8 @@ import ru.practicum.explore.categories.dto.CategoryDto;
 
 public interface CategoryService {
     CategoryDto add(CategoryDto categoryDto);
+
     void  delete(Long catId);
+
     CategoryDto update(CategoryDto categoryDto);
 }
